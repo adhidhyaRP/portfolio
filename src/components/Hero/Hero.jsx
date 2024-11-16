d59878e48d0d9eb13cf250eefe759b8501572eda
adhidhyaRP <153973098+adhidhyaRP@users.noreply.github.com>
2024-11-16 17:14:59 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -9,11 +8,21 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Adhidhya</h1>
         <p className={styles.description}>
-        I am a MERN fullstack developer with a passion for building dynamic, responsive, and user-friendly web applications. With a strong foundation in MongoDB, Express.js, React, and Node.js
+          I am a MERN fullstack developer with a passion for building dynamic, responsive, and user-friendly web applications. With a strong foundation in MongoDB, Express.js, React, and Node.js.
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.buttonGroup}>
+          <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1VbfzEjRNSyNjvcS9eCF-kXZpAMsUtC9Z/view"
+            className={styles.resumeBtn}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
